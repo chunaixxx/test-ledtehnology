fa32873f338d9b001ea29bde0fa55d1031c0a0ce
Daniil <chunaixxx@gmail.com>
2021-08-11 01:50:37 +0300

    Initial project setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<main class="main"></main>
+
+	<aside class="aside"></aside>
+</template>
+
+<script>
+export default {
+	name: 'App'
+}
+</script>
+
+<style lang="scss">
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+#app {
+	min-height: 100vh;
+	display: grid;
+	grid-template-columns: 1fr 300px;
+	padding: 30px;
+}
+</style>
